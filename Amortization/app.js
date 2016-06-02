076f38cc0554c80798cfdb7dedb382bace0d675b
Otoniel Carlos <otonielcarlosf@gmail.com>
2016-06-01 20:22:01 -0500

    cambio de opciones

--- Amortization/app.js
+++ Amortization/app.js
@@ -4,11 +4,26 @@ app.controller("MainCtrl", ["$scope", function ($scope) {
 
     $scope.loan_amount = null;
     $scope.interest_rate = null;
-    $scope.months = null;
+//    $scope.months = null;
     $scope.pmt = null;
     $scope.selected = null;
     $scope.output = null;
 
+    $scope.meses = [
+        {'month': '6'},
+        {'month': '12'},
+        {'month': '18'}
+    ];
+
+    $scope.codigo = [
+        {'lookupCode': '6', 'description': '6 Meses'},
+        {'lookupCode': '12', 'description': '12 Meses'},
+        {'lookupCode': '18', 'description': '18 Meses'}
+    ];    
+//        $scope.myMonth = $scope.months;
+
+ 
+    
     $scope.calculate = function (rate, nper, pv) {
 
         var pvif = Math.pow(1 + rate, nper);
